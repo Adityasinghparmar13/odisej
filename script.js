8b6b35c56e763e9392e49d178ddc1c79a6ee06f9
test1 <138242529+Adityasinghparmar13@users.noreply.github.com>
2024-12-15 11:37:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var tot = document.querySelectorAll("#page2 h1")
+tot.forEach(function(elem){
+  var colh1 = elem.textContent
+  var splittxt = colh1.split("")
+  var clutter= ""
+  splittxt.forEach(function(e){
+    clutter+=`<span>${e}</span>`;
+  })
+  console.log(clutter)
+  elem.innerHTML = clutter
+})
+
+gsap.to("#page2 h1 span",{
+    color: "#E3E3C4",
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:"#page2 h1",
+        scroller: "body",
+        start:"top 20%",
+        end:"top -10%",
+        scrub:2,
+    }
+})
